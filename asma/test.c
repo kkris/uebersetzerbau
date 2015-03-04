12080546113628765742f8209f44ce7f1a98b5e4
Kristoffer Kleine <kris.kleine@yahoo.de>
2015-03-04 19:16:54 +0100

    Improve tests

--- asma/test.c
+++ asma/test.c
@@ -2,20 +2,16 @@
 
 #include "asma.h"
 
-int run_test(unsigned long x0, unsigned long x1,
-             unsigned long y0, unsigned long y1,
-             unsigned long r0, unsigned long r1)
+void run_test(unsigned long x0, unsigned long x1,
+              unsigned long y0, unsigned long y1,
+              unsigned long r[])
 {
-    unsigned long x[2], y[2], r[2];
+    unsigned long x[2], y[2];
 
     x[0] = x0; x[1] = x1;
     y[0] = y0; y[1] = y1;
 
     asma(x, y, r);
-
-    printf("[%ld, %ld]\n", r[0], r[1]);
-
-    return r[0] == r0 && r[1] == r1;
 }
 
 int main()
--- asma/test.py
+++ asma/test.py
@@ -1,3 +1,5 @@
+import sys
+import random
 from cffi import FFI
 
 ffi = FFI()
@@ -5,9 +7,35 @@ ffi = FFI()
 ffi.cdef("""
     int run_test(unsigned long x0, unsigned long x1,
                  unsigned long y0, unsigned long y1,
-                 unsigned long r0, unsigned long r1);
+                 unsigned long r[]);
 """)
 
-lib = ffi.dlopen("./asma_reference.so")
+reference_lib = ffi.dlopen("./asma_reference.so")
+implementation_lib = ffi.dlopen("./asma_impl.so")
 
-lib.run_test(1, 2, 3, 4, 5, 6)
+
+test_cases = []
+
+MAX = 2 ** 20
+
+for _ in range(int(sys.argv[1])):
+    test_cases.append([
+        random.randint(0, MAX),
+        random.randint(0, MAX),
+        random.randint(0, MAX),
+        random.randint(0, MAX)
+    ])
+
+
+r = ffi.new("unsigned long[]", 2)
+
+for t in test_cases:
+    reference_lib.run_test(t[0], t[1], t[2], t[3], r)
+
+    r0 = r[0]
+    r1 = r[1]
+
+    implementation_lib.run_test(t[0], t[1], t[2], t[3], r)
+
+    assert r0 == r[0] and r1 == r[1]
+
