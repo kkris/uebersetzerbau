1e4ce4979d1e0371bb8964cbe73d33cf96e4de05
Kristoffer Kleine <kris.kleine@yahoo.de>
2015-03-12 12:13:56 +0100

    Further reduce instructions in loop

--- asmb/solution.s
+++ asmb/solution.s
@@ -5,27 +5,23 @@ asmb:
 .LFB0:
 	.cfi_startproc
     mov $0, %r8
+    mov %rcx, %r9
     clc # set cf flag to zero?
-
-    movq $0, %r10
 
     testq %rcx, %rcx # n = 0
     je .loopend
 
 .loopbody:
-    bt $0, %r10 # load carry flag from r10
-
     movq    (%rdi, %r8, 8), %rax
     sbbq    (%rsi, %r8, 8), %rax
     movq    %rax, (%rdx, %r8, 8)
 
-    sbbq    %r10, %r10 # store borrow in r10 (0 or -1)
-
     inc %r8
-    cmpq    %rcx, %r8
-    jne .loopbody
+    dec %r9
+    jnz .loopbody
 .loopend:
-    movq  %r10, (%rdx, %rcx, 8)
+    sbbq %r10, %r10
+    movq %r10, (%rdx, %rcx, 8)
 	ret
 	.cfi_endproc
 .LFE0:
