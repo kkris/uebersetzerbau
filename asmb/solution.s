e539c03257cdcd3a9d0c4eeebe9e2c0e4a98c830
Kristoffer Kleine <kris.kleine@yahoo.de>
2015-03-12 22:10:25 +0100

    Down to 5 instructions per loop

--- asmb/solution.s
+++ asmb/solution.s
@@ -5,22 +5,19 @@ asmb:
 .LFB0:
 	.cfi_startproc
     movq $0, %r8
-    movq %rcx, %r9
 
     testq %rcx, %rcx # test if n = 0, carry flag is cleared
     je .loopend
-
 .loopbody:
     movq    (%rdi, %r8, 8), %rax
     sbbq    (%rsi, %r8, 8), %rax
     movq    %rax, (%rdx, %r8, 8)
 
     inc %r8
-    dec %r9
-    jnz .loopbody
+    loop .loopbody
 .loopend:
     sbbq %r10, %r10
-    movq %r10, (%rdx, %rcx, 8)
+    movq %r10, (%rdx, %r8, 8)
 	ret
 	.cfi_endproc
 .LFE0:
