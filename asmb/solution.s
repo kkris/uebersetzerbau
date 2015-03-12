48e886764e236032266805deca6bda7273020ded
Kristoffer Kleine <kris.kleine@yahoo.de>
2015-03-12 22:20:24 +0100

    Rename labels

--- asmb/solution.s
+++ asmb/solution.s
@@ -7,15 +7,15 @@ asmb:
     movq $0, %r8
 
     testq %rcx, %rcx # test if n = 0, carry flag is cleared
-    je .loopend
-.loopbody:
+    je .epilog
+.loop:
     movq    (%rdi, %r8, 8), %rax
     sbbq    (%rsi, %r8, 8), %rax
     movq    %rax, (%rdx, %r8, 8)
 
     inc %r8
-    loop .loopbody
-.loopend:
+    loop .loop
+.epilog:
     sbbq %r10, %r10
     movq %r10, (%rdx, %r8, 8)
 	ret
